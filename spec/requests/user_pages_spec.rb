e2f2850a245db64e7e0b69f57ecfc46965face49
Michael Hauser-Raspe (home) <michael.hauser-raspe@cantab.net>
2013-05-31 13:17:55 +0100

    chap 8

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,8 +28,16 @@ describe "UserPages" do
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
+      
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
       end
     end
 
